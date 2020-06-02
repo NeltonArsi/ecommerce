9012510b6611a90fd05023383c4f5cbb934fc8a8
Narsi <neltonarsi@gmail.com>
2020-06-01 22:18:06 -0300

    Tela de login

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 
-require_once "vendor/autoload.php";
+session_start();
 
+require_once "vendor/autoload.php";
+require_once "functions.php";
+
+use \Hcode\Model\User;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Slim\Slim;
@@ -19,8 +23,37 @@ $app->get('/', function () {
 
 $app->get('/admin', function () {
 
+	User::verifyLogin();
 	$page = new PageAdmin();
 	$page->setTpl("index");
+
+});
+
+$app->get('/admin/login', function () {
+
+	$page = new PageAdmin([
+		"header" => false,
+		"footer" => false,
+	]);
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function () {
+
+	User::login(post('deslogin'), post('despassword'));
+
+	header("Location: /admin");
+	exit;
+
+});
+
+$app->get('/admin/logout', function () {
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
 
 });
 
