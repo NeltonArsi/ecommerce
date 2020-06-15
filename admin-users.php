28baa5a6fbdb116b6e72e69fe8aa84db5f992669
Narsi <neltonarsi@gmail.com>
2020-06-15 12:21:14 -0300

    Admin User - Search And Pagination

--- admin-users.php
+++ admin-users.php
@@ -3,25 +3,6 @@
 use \Hcode\Model\User;
 use \Hcode\PageAdmin;
 
-$app->get('/admin/users', function () {
-
-	User::verifyLogin();
-	$users = User::listAll();
-	$page = new PageAdmin();
-	$page->setTpl("users", array(
-		"users" => $users,
-	));
-
-});
-
-$app->get('/admin/users/create', function () {
-
-	User::verifyLogin();
-	$page = new PageAdmin();
-	$page->setTpl("users-create");
-
-});
-
 $app->get('/admin/users/:iduser/delete', function ($iduser) {
 
 	User::verifyLogin();
@@ -46,6 +27,28 @@ $app->get('/admin/users/:iduser', function ($iduser) {
 
 });
 
+$app->post('/admin/users/:iduser', function ($iduser) {
+
+	User::verifyLogin();
+	$user = new User();
+	$_POST["inadmin"] = (isset($_POST["inadmin"])) ? 1 : 0;
+	$user->get((int) $iduser);
+	$user->setData($_POST);
+	$user->update();
+
+	header("Location: /admin/users");
+	exit;
+
+});
+
+$app->get('/admin/users/create', function () {
+
+	User::verifyLogin();
+	$page = new PageAdmin();
+	$page->setTpl("users-create");
+
+});
+
 $app->post('/admin/users/create', function () {
 
 	User::verifyLogin();
@@ -59,17 +62,39 @@ $app->post('/admin/users/create', function () {
 
 });
 
-$app->post('/admin/users/:iduser', function ($iduser) {
+$app->get('/admin/users', function () {
 
 	User::verifyLogin();
-	$user = new User();
-	$_POST["inadmin"] = (isset($_POST["inadmin"])) ? 1 : 0;
-	$user->get((int) $iduser);
-	$user->setData($_POST);
-	$user->update();
 
-	header("Location: /admin/users");
-	exit;
+	$search = (isset($_GET['search'])) ? $_GET['search'] : "";
+	$page = (isset($_GET['page'])) ? (int) $_GET['page'] : 1;
+
+	if ($search != '') {
+		$pagination = User::getPageSearch($search, $page);
+	} else {
+		$pagination = User::getPage($page);
+	}
+
+	$pages = [];
+
+	for ($x = 0; $x < $pagination['pages']; $x++) {
+		array_push($pages, [
+			'href' => '/admin/users?' . http_build_query([
+				'page' => $x + 1,
+				'search' => $search,
+			]),
+			'text' => $x + 1,
+		]);
+
+	}
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users", array(
+		"users" => $pagination['data'],
+		"search" => $search,
+		"pages" => $pages,
+	));
 
 });
 
