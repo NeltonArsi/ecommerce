bf496d100d98a6dbf9fc574c067adaf54d6976b7
Narsi <neltonarsi@gmail.com>
2020-06-17 18:40:41 -0300

    Integração PagSeguro HTML

--- site.php
+++ site.php
@@ -240,17 +240,60 @@ $app->post("/checkout", function () {
 	]);
 	$order->save();
 
-/**	switch ((int)$_POST['payment-method']) {
-		case 1:
+	//switch ((int)$_POST['payment-method']) {
+		//case 1:
 		header("Location: /order/".$order->getidorder()."/pagseguro");
-		break;
+		//break;
 
-		case 2:
-		header("Location: /order/".$order->getidorder()."/paypal");
-		break;
-	}**/
-	header("Location: /order/".$order->getidorder());
+		//case 2:
+		//header("Location: /order/".$order->getidorder()."/paypal");
+		//break;
+	//}
+	//header("Location: /order/".$order->getidorder());
 	exit;
+
+});
+
+$app->get("/order/:idorder/pagseguro", function($idorder){
+
+	User::verifyLogin(false);
+	$order = new Order();
+	$order->get((int)$idorder);
+	$cart = $order->getCart();
+	$page = new Page([
+		'header'=>false,
+		'footer'=>false
+	]);
+
+	$page->setTpl("payment-pagseguro", [
+		'order'=>$order->getValues(),
+		'cart'=>$cart->getValues(),
+		'products'=>$cart->getProducts(),
+		'phone'=>[
+			'areaCode'=>substr($order->getnrphone(), 0, 2),
+			'number'=>substr($order->getnrphone(), 2, strlen($order->getnrphone()))
+		]
+	]);
+
+
+});
+
+$app->get("/order/:idorder/paypal", function($idorder){
+
+	User::verifyLogin(false);
+	$order = new Order();
+	$order->get((int)$idorder);
+	$cart = $order->getCart();
+	$page = new Page([
+		'header'=>false,
+		'footer'=>false
+	]);
+
+	$page->setTpl("payment-paypal", [
+		'order'=>$order->getValues(),
+		'cart'=>$cart->getValues(),
+		'products'=>$cart->getProducts()
+	]);
 
 });
 
